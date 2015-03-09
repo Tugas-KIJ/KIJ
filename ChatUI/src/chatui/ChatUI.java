f5847fc210f3d22e4f012a7921c31ca35be8aaf3
evariaayu <evariaayu@gmail.com>
2015-03-09 14:23:50 +0700

    gui awal

---
+++ ChatUI/src/chatui/ChatUI.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chatui;
+
+/**
+ *
+ * @author evaria
+ */
+public class ChatUI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
