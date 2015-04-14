a5e6cfccf18063ea45d07b949df3c3f15787bd79
Vancrew <im26vancrew@gmail.com>
2015-04-15 01:02:30 +0700

    DES.java

---
+++ ChatUI/src/chatui/DES.java
@@ -0,0 +1,359 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chatui;
+
+/**
+ *
+ * @author Vancrew
+ */
+public class DES {
+    
+    public DES(){
+        
+    }
+    
+    private static int[] PC1 = {
+        57, 49, 41, 33, 25, 17,  9,
+         1, 58, 50, 42, 34, 26, 18,
+        10,  2, 59, 51, 43, 35, 27,
+        19, 11,  3, 60, 52, 44, 36,
+        63, 55, 47, 39, 31, 23, 15,
+         7, 62, 54, 46, 38, 30, 22,
+        14,  6, 61, 53, 45, 37, 29,
+        21, 13,  5, 28, 20, 12,  4
+    };
+    
+    private static int[] Shift = {
+        1, 1, 2, 2, 2, 2, 2, 2,
+        1, 2, 2, 2, 2, 2, 2, 1
+    };
+    
+    private static int[] PC2 = {
+        14, 17, 11, 24,  1,  5,  3, 28,
+        15,  6, 21, 10, 23, 19,	12,  4,	
+        26,  8, 16,  7, 27, 20,	13,  2,
+        41, 52,	31, 37, 47, 55, 30, 40,
+        51, 45,	33, 48, 44, 49,	39, 56,	
+        34, 53, 46, 42, 50, 36,	29, 32  
+    };
+    
+    private static int[] IP = {
+        58, 50, 42, 34, 26, 18, 10, 2,
+        60, 52,	44, 36,	28, 20,	12, 4,
+        62, 54,	46, 38,	30, 22,	14, 6,
+        64, 56,	48, 40,	32, 24,	16, 8,
+        57, 49,	41, 33,	25, 17,	 9, 1,
+        59, 51,	43, 35,	27, 19,	11, 3,
+        61, 53,	45, 37,	29, 21,	13, 5,
+        63, 55,	47, 39,	31, 23,	15, 7  
+    };
+    
+    private static int[] Expansion = {
+        32,  1,  2,  3,  4,  5,
+         4,  5,	 6,  7,	 8,  9,
+         8,  9,	10, 11,	12, 13,
+        12, 13,	14, 15,	16, 17,
+        16, 17,	18, 19,	20, 21,
+        20, 21,	22, 23,	24, 25,
+        24, 25,	26, 27,	28, 29,
+        28, 29,	30, 31,	32,  1				
+    };
+    
+    private static int[] P = {
+        16,  7,	20, 21,
+        29, 12,	28, 17,
+         1, 15,	23, 26,
+         5, 18,	31, 10,
+         2,  8,	24, 14,
+        32, 27,	 3,  9,
+        19, 13,	30,  6,
+        22, 11,	 4, 25,  
+    };
+    
+    private static int[] IP_ = {
+        40,  8,	48, 16,	56, 24,	64, 32,
+        39,  7,	47, 15,	55, 23,	63, 31,
+        38,  6,	46, 14,	54, 22,	62, 30,
+        37,  5,	45, 13,	53, 21,	61, 29,
+        36,  4,	44, 12,	52, 20,	60, 28,
+        35,  3,	43, 11,	51, 19,	59, 27,
+        34,  2,	42, 10,	50, 18,	58, 26,
+        33,  1,	41,  9,	49, 17,	57, 25
+    };
+    
+    private static int[][][] S = new int[][][]
+    {   {   {14,  4, 13,  1,  2, 15, 11,  8,  3, 10,  6, 12,  5,  9,  0,  7}, { 0, 15,  7,  4, 14,  2, 13,  1, 10,  6, 12, 11,  9,  5,  3,  8}, { 4,  1, 14,  8, 13,  6,  2, 11, 15, 12,  9,  7,  3, 10,  5,  0}, {15, 12,  8,  2,  4,  9,  1,  7,  5, 11,  3, 14, 10,  0,  6, 13}  },
+        {
+        {15,	1,	8,	14,	6,	11,	3,	4,	9,	7,	2,	13,	12,	0,	5,	10},
+        {3,	13,	4,	7,	15,	2,	8,	14,	12,	0	,1,	10,	6,	9,	11,	5},
+        {0,	14,	7,	11,	10,	4,	13,	1,	5,	8,	12,	6,	9,	3,	2,	15},
+        {13,	8,	10,	1	,3	,15,	4,	2	,11	,6	,7,	12,	0,	5,	14,	9}
+    },
+        {
+        {10,	0	,9,	14,	6,	3,	15,	5,	1,	13,	12,	7,	11,	4,	2,	8},
+        {13,	7,	0,	9,	3,	4,	6,	10,	2,	8,	5,	14,	12,	11,	15,	1},
+        {13,	6,	4,	9,	8,	15,	3	,0,	11,	1,	2,	12,	5	,10,	14,	7},
+        {1,	10,	13,	0,	6,	9,	8,	7,	4,	15,	14,	3,	11,	5,	2,	12}
+    },
+        {
+        {7,	13,	14,	3	,0,	6,	9,	10,	1,	2,	8,	5,	11,	12,	4,	15},
+        {13,	8,	11,	5,	6,	15,	0,	3,	4,	7,	2,	12,	1,	10,	14,	9},
+        {10,	6,	9,	0,	12	,11,	7,	13,	15,	1,	3,	14,	5,	2,	8,	4},
+        {3,	15,	0,	6,	10,	1,	13,	8,	9,	4,	5,	11,	12,	7,	2,	14}
+    },
+        {
+        {2,	12,	4,	1,	7,	10,	11,	6,	8,	5,	3,	15,	13,	0,	14,	9},
+        {14,	11,	2,	12,	4,	7,	13,	1,	5,	0,	15,	10,	3,	9,	8,	6},
+        {4,	2,	1,	11,	10,	13,	7,	8,	15,	9,	12,	5,	6,	3,	0,	14},
+        {11,	8,	12,	7,	1,	14,	2,	13,	6,	15,	0,	9,	10,	4,	5,	3}
+    },
+        {
+        {12,	1,	10,	15,	9,	2,	6,	8,	0,	13,	3,	4,	14,	7,	5,	11},
+        {10,	15,	4,	2,	7,	12,	9,	5,	6,	1,	13,	14,	0,	11,	3,	8},
+        {9,	14,	15,	5,	2,	8,	12,	3,	7,	0,	4,	10,	1,	13,	11,	6},
+        {4,	3,	2,	12,	9,	5,	15,	10,	11,	14,	1,	7,	6,	0,	8,	13}
+    },
+        {
+        {4,	11,	2,	14,	15,	0,	8,	13,	3,	12,	9,	7,	5,	10,	6,	1},
+        {13,	0,	11,	7,	4,	9,	1,	10,	14,	3,	5,	12,	2,	15,	8,	6},
+        {1,	4,	11,	13,	12,	3,	7,	14,	10,	15,	6,	8,	0,	5,	9,	2},
+        {6,	11,	13,	8,	1,	4,	10,	7,	9,	5,	0,	15,	14,	2,	3,	12}
+    },
+        {
+        {13,	2,	8,	4,	6,	15,	11,	1,	10,	9	,3	,14,	5,	0,	12,	7},
+        {1,	15,	13,	8,	10	,3,	7,	4,	12,	5,	6,	11,	0,	14,	9,	2},
+        {7,	11,	4,	1,	9,	12,	14,	2,	0	,6	,10,	13,	15,	3,	5,	8},
+        {2,	1,	14,	7,	4,	10,	8,	13,	15,	12,	9,	0,	3,	5,	6,	11}
+    },
+            
+    };
+    /*
+    private static int[][] S1 = new int[][]{
+        {14,  4, 13,  1,  2, 15, 11,  8,  3, 10,  6, 12,  5,  9,  0,  7},
+        { 0, 15,  7,  4, 14,  2, 13,  1, 10,  6, 12, 11,  9,  5,  3,  8},
+        { 4,  1, 14,  8, 13,  6,  2, 11, 15, 12,  9,  7,  3, 10,  5,  0},
+        {15, 12,  8,  2,  4,  9,  1,  7,  5, 11,  3, 14, 10,  0,  6, 13}
+    };
+    
+    private static  int[][] S2 = new int[][]{
+        {15,	1,	8,	14,	6,	11,	3,	4,	9,	7,	2,	13,	12,	0,	5,	10},
+        {3,	13,	4,	7,	15,	2,	8,	14,	12,	0	,1,	10,	6,	9,	11,	5},
+        {0,	14,	7,	11,	10,	4,	13,	1,	5,	8,	12,	6,	9,	3,	2,	15},
+        {13,	8,	10,	1	,3	,15,	4,	2	,11	,6	,7,	12,	0,	5,	14,	9}
+    };
+    
+    private static  int[][] S3 = new int[][]{
+        {10,	0	,9,	14,	6,	3,	15,	5,	1,	13,	12,	7,	11,	4,	2,	8},
+        {13,	7,	0,	9,	3,	4,	6,	10,	2,	8,	5,	14,	12,	11,	15,	1},
+        {13,	6,	4,	9,	8,	15,	3	,0,	11,	1,	2,	12,	5	,10,	14,	7},
+        {1,	10,	13,	0,	6,	9,	8,	7,	4,	15,	14,	3,	11,	5,	2,	12}
+    };
+    
+    private static  int[][] S4 = new int[][]{
+        {7,	13,	14,	3	,0,	6,	9,	10,	1,	2,	8,	5,	11,	12,	4,	15},
+        {13,	8,	11,	5,	6,	15,	0,	3,	4,	7,	2,	12,	1,	10,	14,	9},
+        {10,	6,	9,	0,	12	,11,	7,	13,	15,	1,	3,	14,	5,	2,	8,	4},
+        {3,	15,	0,	6,	10,	1,	13,	8,	9,	4,	5,	11,	12,	7,	2,	14}
+    };
+    
+    private static  int[][] S5 = new int[][]{
+        {2,	12,	4,	1,	7,	10,	11,	6,	8,	5,	3,	15,	13,	0,	14,	9},
+        {14,	11,	2,	12,	4,	7,	13,	1,	5,	0,	15,	10,	3,	9,	8,	6},
+        {4,	2,	1,	11,	10,	13,	7,	8,	15,	9,	12,	5,	6,	3,	0,	14},
+        {11,	8,	12,	7,	1,	14,	2,	13,	6,	15,	0,	9,	10,	4,	5,	3}
+    };
+    
+    private static  int[][] S6 = new int[][]{
+        {12,	1,	10,	15,	9,	2,	6,	8,	0,	13,	3,	4,	14,	7,	5,	11},
+        {10,	15,	4,	2,	7,	12,	9,	5,	6,	1,	13,	14,	0,	11,	3,	8},
+        {9,	14,	15,	5,	2,	8,	12,	3,	7,	0,	4,	10,	1,	13,	11,	6},
+        {4,	3,	2,	12,	9,	5,	15,	10,	11,	14,	1,	7,	6,	0,	8,	13}
+    };
+    
+    private static  int[][] S7 = new int[][]{
+        {4,	11,	2,	14,	15,	0,	8,	13,	3,	12,	9,	7,	5,	10,	6,	1},
+        {13,	0,	11,	7,	4,	9,	1,	10,	14,	3,	5,	12,	2,	15,	8,	6},
+        {1,	4,	11,	13,	12,	3,	7,	14,	10,	15,	6,	8,	0,	5,	9,	2},
+        {6,	11,	13,	8,	1,	4,	10,	7,	9,	5,	0,	15,	14,	2,	3,	12}
+    };
+    
+    private static int[][] S8 = new int[][]{
+        {13,	2,	8,	4,	6,	15,	11,	1,	10,	9	,3	,14,	5,	0,	12,	7},
+        {1,	15,	13,	8,	10	,3,	7,	4,	12,	5,	6,	11,	0,	14,	9,	2},
+        {7,	11,	4,	1,	9,	12,	14,	2,	0	,6	,10,	13,	15,	3,	5,	8},
+        {2,	1,	14,	7,	4,	10,	8,	13,	15,	12,	9,	0,	3,	5,	6,	11}
+    };
+    
+   
+    */
+    
+    public static void toBase(int number, int base) {
+        String binary = "";
+        int temp = number/2+1;
+        for (int j = 0; j < temp ; j++) {
+            try {
+                binary += "" + number % base;
+
+                number /= base;
+            } catch (Exception e) {
+            }
+        }
+        for (int j = binary.length() - 1; j >= 0; j--) {
+            System.out.print(binary.charAt(j));
+        }
+    }
+    
+    //Fungsi Subtitusi
+    public static String subtitusi(String a){
+        String g = "";
+       
+        for(int i=1;i<9;i++){
+            String d = ""; 
+            d += a.substring(6*(i-1), 6*i);
+            String h = "";
+            String j = "";
+            h += d.charAt(0);
+            h += d.charAt(5);
+            j += d.substring(1, 5);
+            int k = Integer.parseInt(h, 2);
+            int l = Integer.parseInt(j, 2);
+            //System.out.println(d);
+            //System.out.println(h);
+            //System.out.println(j);
+            //System.out.println("k = " + k + ", l = " + l);
+            //System.out.println(S[i-1][k][l]);
+            System.out.println(Integer.toBinaryString(S[i-1][k][l]));
+            if(S[i-1][k][l]<2)
+            {
+                g += "000" + Integer.toBinaryString(S[i-1][k][l]);
+            }
+            else if(S[i-1][k][l]<4)
+            {
+                g += "00" + Integer.toBinaryString(S[i-1][k][l]);
+            }
+            else if(S[i-1][k][l]<8)
+            {
+                g += "0" + Integer.toBinaryString(S[i-1][k][l]);
+            }
+            else
+            {
+                g += Integer.toBinaryString(S[i-1][k][l]);
+            }
+        }
+        
+        //h +=a.charAt(0);
+        //h +=a.charAt(5);
+        
+        
+       
+        //return Integer.toBinaryString(b[k][l]);
+        return g;
+    }
+    
+    // Fungsi mengubah key (string) menjadi key (binary)
+    public static String keystringbinary(String a){
+    byte[] infoBin;
+    String g = "";
+        infoBin = a.getBytes();
+        for (byte b : infoBin) {
+            g += "0" + Integer.toBinaryString(b);
+        }
+        //System.out.println(g);
+        return g;
+    }
+    
+    //Fungsi Permutasi
+    public static String permutation(String a, int[] b){
+        String g="";
+        for(int c: b){
+            g += a.charAt(c-1);
+        }
+        //System.out.println(g);
+        return g;
+    }
+    
+    public static String XOR(String a, String b){
+        String g="";
+       // System.out.println(a.length());
+        for(int i = 0; i<48;i++){
+            if(a.charAt(i)== b.charAt(i)){
+                g += "0";
+            }
+            else{
+                g += "1";
+            }
+        }
+        return g;
+    }
+    
+    public static String XOR2(String a, String b){
+        String g="";
+       // System.out.println(a.length());
+        for(int i = 0; i<32;i++){
+            if(a.charAt(i)== b.charAt(i)){
+                g += "0";
+            }
+            else{
+                g += "1";
+            }
+        }
+        return g;
+    }
+    
+    public static String LeftShift(String a, int b){
+        String g="";
+        //int i =0;
+        for(int i = 0 ; i <28 ; i++ ){
+          //  System.out.println((i+b)%28 + " " + a.charAt((i+b)%28));
+            g += a.charAt((i+b)%28);
+        }
+        return g;
+    }
+    
+    public static String hex(String a){ 
+    int digitNumber = 1;
+    int sum = 0;
+    String g = "";
+    for(int i = 0; i < a.length(); i++){
+        if(digitNumber == 1)
+            sum+=Integer.parseInt(a.charAt(i) + "")*8;
+        else if(digitNumber == 2)
+            sum+=Integer.parseInt(a.charAt(i) + "")*4;
+        else if(digitNumber == 3)
+            sum+=Integer.parseInt(a.charAt(i) + "")*2;
+        else if(digitNumber == 4 || i < a.length()+1){
+            sum+=Integer.parseInt(a.charAt(i) + "")*1;
+            digitNumber = 0;
+            if(sum < 10)
+                g += sum;
+                //System.out.print(sum);
+            else if(sum == 10)
+                g += "A";
+                //System.out.print("A");
+            else if(sum == 11)
+                g += "B";
+                //System.out.print("B");
+            else if(sum == 12)
+                g += "C";
+                //System.out.print("C");
+            else if(sum == 13)
+                g += "D";
+               // System.out.print("D");
+            else if(sum == 14)
+                g += "E";
+               // System.out.print("E");
+            else if(sum == 15)
+                g += "F";
+                //System.out.print("F");
+            sum=0;
+        }
+        digitNumber++;  
+        //return g;
+    }
+    return g;
+    }
+    
+}
