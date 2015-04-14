5a5506fcd72f090639bcd054040831079c1e9d4b
Mohammad Wahyu Hidayat <im26vancrew@gmail.com>
2015-04-15 01:34:37 +0700

    Update DES.java
    DES fix

--- ChatUI/src/chatui/DES.java
+++ ChatUI/src/chatui/DES.java
@@ -130,84 +130,11 @@ public class DES {
     },
             
     };
-    /*
-    private static int[][] S1 = new int[][]{
-        {14,  4, 13,  1,  2, 15, 11,  8,  3, 10,  6, 12,  5,  9,  0,  7},
-        { 0, 15,  7,  4, 14,  2, 13,  1, 10,  6, 12, 11,  9,  5,  3,  8},
-        { 4,  1, 14,  8, 13,  6,  2, 11, 15, 12,  9,  7,  3, 10,  5,  0},
-        {15, 12,  8,  2,  4,  9,  1,  7,  5, 11,  3, 14, 10,  0,  6, 13}
-    };
     
-    private static  int[][] S2 = new int[][]{
-        {15,	1,	8,	14,	6,	11,	3,	4,	9,	7,	2,	13,	12,	0,	5,	10},
-        {3,	13,	4,	7,	15,	2,	8,	14,	12,	0	,1,	10,	6,	9,	11,	5},
-        {0,	14,	7,	11,	10,	4,	13,	1,	5,	8,	12,	6,	9,	3,	2,	15},
-        {13,	8,	10,	1	,3	,15,	4,	2	,11	,6	,7,	12,	0,	5,	14,	9}
-    };
-    
-    private static  int[][] S3 = new int[][]{
-        {10,	0	,9,	14,	6,	3,	15,	5,	1,	13,	12,	7,	11,	4,	2,	8},
-        {13,	7,	0,	9,	3,	4,	6,	10,	2,	8,	5,	14,	12,	11,	15,	1},
-        {13,	6,	4,	9,	8,	15,	3	,0,	11,	1,	2,	12,	5	,10,	14,	7},
-        {1,	10,	13,	0,	6,	9,	8,	7,	4,	15,	14,	3,	11,	5,	2,	12}
-    };
-    
-    private static  int[][] S4 = new int[][]{
-        {7,	13,	14,	3	,0,	6,	9,	10,	1,	2,	8,	5,	11,	12,	4,	15},
-        {13,	8,	11,	5,	6,	15,	0,	3,	4,	7,	2,	12,	1,	10,	14,	9},
-        {10,	6,	9,	0,	12	,11,	7,	13,	15,	1,	3,	14,	5,	2,	8,	4},
-        {3,	15,	0,	6,	10,	1,	13,	8,	9,	4,	5,	11,	12,	7,	2,	14}
-    };
-    
-    private static  int[][] S5 = new int[][]{
-        {2,	12,	4,	1,	7,	10,	11,	6,	8,	5,	3,	15,	13,	0,	14,	9},
-        {14,	11,	2,	12,	4,	7,	13,	1,	5,	0,	15,	10,	3,	9,	8,	6},
-        {4,	2,	1,	11,	10,	13,	7,	8,	15,	9,	12,	5,	6,	3,	0,	14},
-        {11,	8,	12,	7,	1,	14,	2,	13,	6,	15,	0,	9,	10,	4,	5,	3}
-    };
-    
-    private static  int[][] S6 = new int[][]{
-        {12,	1,	10,	15,	9,	2,	6,	8,	0,	13,	3,	4,	14,	7,	5,	11},
-        {10,	15,	4,	2,	7,	12,	9,	5,	6,	1,	13,	14,	0,	11,	3,	8},
-        {9,	14,	15,	5,	2,	8,	12,	3,	7,	0,	4,	10,	1,	13,	11,	6},
-        {4,	3,	2,	12,	9,	5,	15,	10,	11,	14,	1,	7,	6,	0,	8,	13}
-    };
-    
-    private static  int[][] S7 = new int[][]{
-        {4,	11,	2,	14,	15,	0,	8,	13,	3,	12,	9,	7,	5,	10,	6,	1},
-        {13,	0,	11,	7,	4,	9,	1,	10,	14,	3,	5,	12,	2,	15,	8,	6},
-        {1,	4,	11,	13,	12,	3,	7,	14,	10,	15,	6,	8,	0,	5,	9,	2},
-        {6,	11,	13,	8,	1,	4,	10,	7,	9,	5,	0,	15,	14,	2,	3,	12}
-    };
-    
-    private static int[][] S8 = new int[][]{
-        {13,	2,	8,	4,	6,	15,	11,	1,	10,	9	,3	,14,	5,	0,	12,	7},
-        {1,	15,	13,	8,	10	,3,	7,	4,	12,	5,	6,	11,	0,	14,	9,	2},
-        {7,	11,	4,	1,	9,	12,	14,	2,	0	,6	,10,	13,	15,	3,	5,	8},
-        {2,	1,	14,	7,	4,	10,	8,	13,	15,	12,	9,	0,	3,	5,	6,	11}
-    };
-    
-   
-    */
-    
-    public static void toBase(int number, int base) {
-        String binary = "";
-        int temp = number/2+1;
-        for (int j = 0; j < temp ; j++) {
-            try {
-                binary += "" + number % base;
 
-                number /= base;
-            } catch (Exception e) {
-            }
-        }
-        for (int j = binary.length() - 1; j >= 0; j--) {
-            System.out.print(binary.charAt(j));
-        }
-    }
     
     //Fungsi Subtitusi
-    public static String subtitusi(String a){
+    private static String subtitusi(String a){
         String g = "";
        
         for(int i=1;i<9;i++){
@@ -220,12 +147,6 @@ public class DES {
             j += d.substring(1, 5);
             int k = Integer.parseInt(h, 2);
             int l = Integer.parseInt(j, 2);
-            //System.out.println(d);
-            //System.out.println(h);
-            //System.out.println(j);
-            //System.out.println("k = " + k + ", l = " + l);
-            //System.out.println(S[i-1][k][l]);
-            System.out.println(Integer.toBinaryString(S[i-1][k][l]));
             if(S[i-1][k][l]<2)
             {
                 g += "000" + Integer.toBinaryString(S[i-1][k][l]);
@@ -243,41 +164,32 @@ public class DES {
                 g += Integer.toBinaryString(S[i-1][k][l]);
             }
         }
-        
-        //h +=a.charAt(0);
-        //h +=a.charAt(5);
-        
-        
-       
-        //return Integer.toBinaryString(b[k][l]);
+
         return g;
     }
     
     // Fungsi mengubah key (string) menjadi key (binary)
-    public static String keystringbinary(String a){
+    private static String keystringbinary(String a){
     byte[] infoBin;
     String g = "";
         infoBin = a.getBytes();
         for (byte b : infoBin) {
             g += "0" + Integer.toBinaryString(b);
         }
-        //System.out.println(g);
         return g;
     }
     
     //Fungsi Permutasi
-    public static String permutation(String a, int[] b){
+    private static String permutation(String a, int[] b){
         String g="";
         for(int c: b){
             g += a.charAt(c-1);
         }
-        //System.out.println(g);
         return g;
     }
     
-    public static String XOR(String a, String b){
+    private static String XOR(String a, String b){
         String g="";
-       // System.out.println(a.length());
         for(int i = 0; i<48;i++){
             if(a.charAt(i)== b.charAt(i)){
                 g += "0";
@@ -289,9 +201,8 @@ public class DES {
         return g;
     }
     
-    public static String XOR2(String a, String b){
+    private static String XOR2(String a, String b){
         String g="";
-       // System.out.println(a.length());
         for(int i = 0; i<32;i++){
             if(a.charAt(i)== b.charAt(i)){
                 g += "0";
@@ -303,17 +214,16 @@ public class DES {
         return g;
     }
     
-    public static String LeftShift(String a, int b){
+    private static String LeftShift(String a, int b){
         String g="";
         //int i =0;
         for(int i = 0 ; i <28 ; i++ ){
-          //  System.out.println((i+b)%28 + " " + a.charAt((i+b)%28));
             g += a.charAt((i+b)%28);
         }
         return g;
     }
     
-    public static String hex(String a){ 
+    private static String hex(String a){ 
     int digitNumber = 1;
     int sum = 0;
     String g = "";
@@ -329,31 +239,157 @@ public class DES {
             digitNumber = 0;
             if(sum < 10)
                 g += sum;
-                //System.out.print(sum);
+         
             else if(sum == 10)
                 g += "A";
-                //System.out.print("A");
+         
             else if(sum == 11)
                 g += "B";
-                //System.out.print("B");
+         
             else if(sum == 12)
                 g += "C";
-                //System.out.print("C");
+           
             else if(sum == 13)
                 g += "D";
-               // System.out.print("D");
+           
             else if(sum == 14)
                 g += "E";
-               // System.out.print("E");
+           
             else if(sum == 15)
                 g += "F";
-                //System.out.print("F");
             sum=0;
         }
         digitNumber++;  
-        //return g;
     }
     return g;
+    }
+    
+    public static String Enkripsi(String plain, String key){
+        String cipher;
+        
+        String plainbinary;
+        String plainip;
+        String[] PL = new String[17];
+        String[] PR = new String[17];
+        String[] EPR = new String[17];
+        String[] KEPR = new String[17];
+        String[] SKEPR = new String[17];
+        String[] PSKEPR = new String[17];
+        String PLPR;
+        String PPLPR;
+ 
+        String bkey;
+        String pcbkey;
+        String[] L = new String[17];
+        String[] R = new String[17];
+        String[] LR = new String[17];
+        String[] LRF = new String[17];
+        
+                
+        bkey = keystringbinary(key);
+        pcbkey = permutation(bkey, PC1);
+        
+        L[0] = pcbkey.substring(0,28);
+        R[0] = pcbkey.substring(28,56);
+        
+        for(int i=1; i<17;i++){
+            L[i] = LeftShift(L[i-1],Shift[i-1]);
+            R[i] = LeftShift(R[i-1],Shift[i-1]);
+        }
+        
+        for(int i=1; i<17;i++){
+            LR[i] = "";
+            LR[i] += L[i] + R[i];
+        }
+        
+        for(int i=1; i<17;i++){
+           LRF[i] = permutation(LR[i], PC2);
+        }
+        
+        plainbinary = keystringbinary(plain);
+        plainip = permutation(plainbinary, IP);
+        
+        PL[0] = plainip.substring(0,32);
+        PR[0] = plainip.substring(32,64);
+        
+        for(int z=0; z<16; z++){
+            EPR[z] = permutation(PR[z], Expansion);
+            KEPR[z] = XOR(LRF[z+1],EPR[z]);
+            SKEPR[z] = subtitusi(KEPR[z]);
+            PSKEPR[z] = permutation(SKEPR[z], P);
+            PL[z+1]= PR[z];
+            PR[z+1]= XOR2(PSKEPR[z],PL[z]);
+        }
+        
+        PLPR = "";
+        PLPR += PR[16] + PL[16];
+        PPLPR = permutation(PLPR, IP_);
+        
+        return PPLPR;
+    }
+    
+    public static String Dekripsi(String plain, String key){
+        String cipher;
+        
+        String plainbinary;
+        String plainip;
+        String[] PL = new String[17];
+        String[] PR = new String[17];
+        String[] EPR = new String[17];
+        String[] KEPR = new String[17];
+        String[] SKEPR = new String[17];
+        String[] PSKEPR = new String[17];
+        String PLPR;
+        String PPLPR;
+ 
+        String bkey;
+        String pcbkey;
+        String[] L = new String[17];
+        String[] R = new String[17];
+        String[] LR = new String[17];
+        String[] LRF = new String[17];
+        
+                
+        bkey = keystringbinary(key);
+        pcbkey = permutation(bkey, PC1);
+        
+        L[0] = pcbkey.substring(0,28);
+        R[0] = pcbkey.substring(28,56);
+        
+        for(int i=1; i<17;i++){
+            L[i] = LeftShift(L[i-1],Shift[i-1]);
+            R[i] = LeftShift(R[i-1],Shift[i-1]);
+        }
+        
+        for(int i=1; i<17;i++){
+            LR[i] = "";
+            LR[i] += L[i] + R[i];
+        }
+        
+        for(int i=1; i<17;i++){
+           LRF[i] = permutation(LR[i], PC2);
+        }
+        
+        plainbinary = keystringbinary(plain);
+        plainip = permutation(plainbinary, IP);
+        
+        PL[0] = plainip.substring(0,32);
+        PR[0] = plainip.substring(32,64);
+        
+        for(int z=0; z<16; z++){
+            EPR[z] = permutation(PR[z], Expansion);
+            KEPR[z] = XOR(LRF[16-z],EPR[z]);
+            SKEPR[z] = subtitusi(KEPR[z]);
+            PSKEPR[z] = permutation(SKEPR[z], P);
+            PL[z+1]= PR[z];
+            PR[z+1]= XOR2(PSKEPR[z],PL[z]);
+        }
+        
+        PLPR = "";
+        PLPR += PR[16] + PL[16];
+        PPLPR = permutation(PLPR, IP_);
+        
+        return PPLPR;
     }
     
 }
